f78f04d919f8c26e8b2d3272961d24022669ba99
Lucas Robson <lucassrobson06@gmail.com>
2023-07-17 16:20:37 -0300

    Atualizando Models do produto

--- produto/models.py
+++ produto/models.py
@@ -2,21 +2,15 @@ from django.db import models
 
 class produto(models.Model):
 
-    OPCOES = (
-        ('Disponivel', 'Disponivel'),
-        ('Alocado', 'Alocado'),
-        ('Quebrado','Quebrado'),
-        ('Manutenção','Manutenção')
-    )
+   
     importado = models.BooleanField(default=False)
-    numero_serie = models.CharField('SERIE', max_length=15)
+    ncm = models.CharField('NCM', max_length=8)
     produto = models.CharField(max_length=100, unique=True)
-    tombo = models.DecimalField(max_digits=5, decimal_places=0)
-    responsavel = models.CharField(max_length=50)
-    status = models.CharField(
-        max_length=10,
-        choices= OPCOES,
-    )
+    preco = models.DecimalField('preço' max_digits=7, decimal_places=2)
+    estoque = models.IntegerField('estoque atual')
+    estoque_minimo = models.PositiveIntegerField('estoque mínimo', default=0)
+    data = models.DateField(null=True, blank=True)
+    
     class Meta:
         ordering=('produto',)
 
