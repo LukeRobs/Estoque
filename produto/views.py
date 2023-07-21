5ee9be763eb036032cec1cedeabadf9b87f1fb0d
Lucas Robson <lucassrobson06@gmail.com>
2023-07-21 16:16:28 -0300

    criando tabela de produtos

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Produto
 
-# Create your views here.
+def produto_list(request):
+    template_name = 'produto_list.html'
+    objects = Produto.objects.all()
+    print(objects)
+    context = {'produtos': objects}
+    return render(request, template_name, context)
