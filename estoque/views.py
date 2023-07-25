59df5c8fd4f7908a1a9c7b510372f0adb10c9fd6
Lucas Robson <lucassrobson06@gmail.com>
2023-07-25 17:10:26 -0300

    detail estoque entrada

--- estoque/views.py
+++ estoque/views.py
@@ -4,7 +4,13 @@ from .models import Estoque
 
 def estoque_entrada_list(request):
     template_name = 'estoque_entrada_list.html'
-   #objects = Estoque.objects.filter(movimento='e')
-    objects = Estoque.objects.all()
+    objects = Estoque.objects.filter(movimento='e')
     context = {'estoque': objects}
+    return render(request, template_name, context)
+
+
+def estoque_entrada_detail(request, pk):
+    template_name = 'estoque_entrada_detail.html'
+    obj = Estoque.objects.get(pk=pk)
+    context = {'object': obj}
     return render(request, template_name, context)
