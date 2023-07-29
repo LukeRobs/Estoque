619fe99526a01d1ab451a77d1b4b79fffe875401
LukeRobs <lucassrobson06@gmail.com>
2023-07-29 10:53:52 -0300

    estoque saida_list

--- estoque/urls.py
+++ estoque/urls.py
@@ -6,5 +6,6 @@ app_name = 'estoque'
 urlpatterns = [
     path('', views.estoque_entrada_list, name='estoque_entrada_list'),
     path('<int:pk>/', views.estoque_entrada_detail, name='estoque_entrada_detail'),
-    path('add/', views.estoque_entrada_add, name='estoque_entrada_add')
+    path('add/', views.estoque_entrada_add, name='estoque_entrada_add'),
+    path('saida/', views.estoque_saida_list, name='estoque_saida_list'),
 ]
